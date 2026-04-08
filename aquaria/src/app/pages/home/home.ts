c96429c5ca619434d8f5953e775747fa0606e305
Alex <alex_hilleshein@estudante.sesisenai.org.br>
2026-04-08 15:11:09 -0300

    .

--- aquaria/src/app/pages/home/home.ts
+++ aquaria/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
