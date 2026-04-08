3d934296279694c94d1d301042f9827271e3f989
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-08 16:21:26 -0300

    .

--- aquaria/src/app/problemas/problemas.ts
+++ aquaria/src/app/problemas/problemas.ts
@@ -8,4 +8,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './problemas.html',
   styleUrl: './problemas.css',
 })
-export class Problemas {}
+export class Problemas {
+  pipi = [
+    {
+      titulo: 'poluiçao, por que aumenta tanto?',
+      imagem: 'https://cienciahoje.org.br/wp-content/uploads/2025/03/AdobeStock_1025686646.jpeg',
+    }
+  ]
+}
