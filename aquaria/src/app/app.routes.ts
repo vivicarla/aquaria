49be03859b59f6b2fda14d671975e64beaee138b
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-08 15:28:36 -0300

    .

--- aquaria/src/app/app.routes.ts
+++ aquaria/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Home } from './pages/home/home';   
 
 export const routes: Routes = [
-    { path: '',component: Home },
+    { path: '', component: Home }
 ];
--- aquaria/src/app/pages/home/home.ts
+++ aquaria/src/app/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +7,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  requisicao = [
+    {
+      not: 'Economia baseada na pesca',
+      descricao: 'Aquária depende quase totalmente da pesca artesanal como principal fonte de renda e alimento para sua população.',
+      imagem: 'https://aquaria.com.br/wp-content/uploads/2023/11/peixe-azul.jpg',
+    },
+    {
+      not: 'Recursos em risco',
+      descricao: 'A pesca excessiva e a poluição estão reduzindo drasticamente a quantidade de peixes, ameaçando o equilíbrio marinho.',
+      imagem: 'https://aquaria.com.br/wp-content/uploads/2023/11/peixe-azul.jpg',
+    },
+    {
+      not: 'Futuro incerto',
+      descricao: 'Sem mudanças urgentes, a economia e o sustento da população podem entrar em colapso.',
+      imagem: 'https://aquaria.com.br/wp-content/uploads/2023/11/peixe-azul.jpg',
+    }
+  ];
+}
