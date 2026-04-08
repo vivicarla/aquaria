94b9e2d4cb98de22930aff96d96a898a31476005
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-08 16:29:41 -0300

    .

--- aquaria/src/app/problemas/problemas.ts
+++ aquaria/src/app/problemas/problemas.ts
@@ -1,18 +1,19 @@
-
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-problemas',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './problemas.html',
   styleUrl: './problemas.css',
 })
 export class Problemas {
   pipi = [
     {
-      titulo: 'poluiçao, por que aumenta tanto?',
+      titulo: 'Sobrepesca',
+      descricao: 'A captura excessiva de peixes impede a reposição natural das espécies. Com o tempo, isso reduz drasticamente a quantidade de recursos disponíveis no oceano.',
       imagem: 'https://cienciahoje.org.br/wp-content/uploads/2025/03/AdobeStock_1025686646.jpeg',
     }
-  ]
+  ];
 }
