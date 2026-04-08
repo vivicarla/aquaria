08d9e953a8a8fe93233c48ed21dc6c6217a654ad
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-08 16:12:44 -0300

    ,

--- aquaria/src/app/app.routes.ts
+++ aquaria/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';   
+import { Problemas } from './problemas/problemas';
 
 export const routes: Routes = [
-    { path: '', component: Home }
+    { path: '', component: Home },
+    { path: 'problemas', component: Problemas }
 ];
