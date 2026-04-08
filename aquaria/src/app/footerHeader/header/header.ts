6dd7c0a4015227b831ac56a35f4fdc4e7f4a6aa2
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-08 15:14:58 -0300

    .

--- aquaria/src/app/footerHeader/header/header.ts
+++ aquaria/src/app/footerHeader/header/header.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
