627b4b69220557ac7780bffdf139d862533dc0b9
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-20 14:26:50 -0300

    .

--- aquaria/src/app/problemas/problemas.ts
+++ aquaria/src/app/problemas/problemas.ts
@@ -15,6 +15,16 @@ export class Problemas {
       descricao: 'A captura excessiva de peixes impede a reposição natural das espécies. Com o tempo, isso reduz drasticamente a quantidade de recursos disponíveis no oceano.',
       imagem: 'https://cienciahoje.org.br/wp-content/uploads/2025/03/AdobeStock_1025686646.jpeg',
     },
+    {
+      titulo: 'Poluição',
+      descricao: 'O descarte inadequado de lixo e resíduos contamina as águas, prejudicando a vida marinha e afetando diretamente a qualidade dos peixes.',
+      imagem: 'https://cienciahoje.org.br/wp-content/uploads/2025/03/AdobeStock_1025686646.jpeg',
+    },
+    {
+      titulo: 'Impacto social',
+      descricao: 'A diminuição dos peixes reduz a renda dos pescadores, aumentando a pobreza e dificultando o acesso a condições básicas de vida.',
+      imagem: 'https://cienciahoje.org.br/wp-content/uploads/2025/03/AdobeStock_1025686646.jpeg',
+    },
   
   ];
 }
