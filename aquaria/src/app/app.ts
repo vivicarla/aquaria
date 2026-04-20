8b8ed0a49602e481f083924532e1b7328125ffbb
Gigi <giovanna_m_moraes@estudante.sesisenai.org.br>
2026-04-20 14:28:27 -0300

    Update app.ts

--- aquaria/src/app/app.ts
+++ aquaria/src/app/app.ts
@@ -4,11 +4,12 @@ import { Home } from './pages/home/home';
 import { Header } from './footerHeader/header/header';
 import { Footer } from './footerHeader/footer/footer';
 import { Problemas } from './problemas/problemas';
+import { Impacto } from './impacto/impacto';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Home, Header, Footer, Problemas],
+  imports: [RouterOutlet, RouterLink, Home, Header, Footer, Problemas, Impacto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
