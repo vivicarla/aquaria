ffd1df66de211e23998df74a12bb322a8cb7f141
Gigi <giovanna_m_moraes@estudante.sesisenai.org.br>
2026-04-20 15:26:45 -0300

    .

--- aquaria/src/app/app.routes.ts
+++ aquaria/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';   
 import { Problemas } from './problemas/problemas';
+import { Impacto } from './impacto/impacto';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'problemas', component: Problemas }
+    { path: 'problemas', component: Problemas },
+    { path: 'impacto', component: Impacto }
 ];
