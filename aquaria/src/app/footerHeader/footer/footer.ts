e72fc2aa7da3d52bfd36d4f81f564cb35690fc39
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-04-20 16:22:07 -0300

    .

--- aquaria/src/app/footerHeader/footer/footer.ts
+++ aquaria/src/app/footerHeader/footer/footer.ts
@@ -8,4 +8,44 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  year: number = new Date().getFullYear();
+
+  contributorsCount: number = 204;
+
+  othersCount: number = 198;
+
+  contributors = [
+    {
+      name: 'Carla Reis',
+      email: 'carla@email.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?auto=format&fit=facearea&w=256&h=256&q=80'
+    },
+    {
+      name: 'Lucas Silva',
+      email: 'lucas@email.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?auto=format&fit=facearea&w=256&h=256&q=80'
+    },
+    {
+      name: 'Ana Costa',
+      email: 'ana@email.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=facearea&w=256&h=256&q=80'
+    },
+    {
+      name: 'Pedro Lima',
+      email: 'pedro@email.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=facearea&w=256&h=256&q=80'
+    },
+    {
+      name: 'Marina Souza',
+      email: 'marina@email.com',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?auto=format&fit=facearea&w=256&h=256&q=80'
+    }
+  ];
+
+}
